98f96bbec5a11f0c2a16f278c45966a3b0513a9d
Fausto Lavezzari <faustolavezzari99@gmail.com>
2022-04-24 16:59:16 -0300

    V0.5.1 (parece que anda)

--- src/Consumidor.java
+++ src/Consumidor.java
@@ -1,10 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.TimeUnit;
 
 
 public class Consumidor extends Actor implements Runnable{
     private Contenedor buffer_validados;
     private Contenedor buffer_inicial;
-
     private int datos_procesados;
 
     public Consumidor(int id, Contenedor buffer_validados, Contenedor buffer_inicial){
@@ -14,18 +15,20 @@ public class Consumidor extends Actor implements Runnable{
         int datos_procesados = 0;
     }
     public void eliminarDato(){
-        int datoID = buffer_validados.getPorConsumir();
-        if(datoID != -1) {
+        Dato eliminar = buffer_validados.getDato(-1); //pide el primer elemento del mapa
+        if(eliminar.getID() != -1) {
             try {                                                                            //tiempo de espera
                 TimeUnit.MILLISECONDS.sleep(timer1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            buffer_inicial.removeDatos(datoID);
-            buffer_validados.removeDatos(datoID);
-            datos_procesados++;
+            if(buffer_inicial.removeDatos(eliminar.getID()) && buffer_validados.removeDatos(eliminar.getID())) {
+                datos_procesados++;
+            }
         }
     }
+    @Override
+    public void putQueue(int datoID){};
     @Override
     public void run() {
         while (datos_procesados < 50){
