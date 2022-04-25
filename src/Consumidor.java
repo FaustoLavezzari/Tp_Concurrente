eefeb29eea5afd87ea1742acf6c1ec74b510ad74
Fausto Lavezzari <faustolavezzari99@gmail.com>
2022-04-25 16:31:17 -0300

    V1.9.0 (log en file)

--- src/Consumidor.java
+++ src/Consumidor.java
@@ -26,7 +26,6 @@ public class Consumidor extends Actor implements Runnable{
                 e.printStackTrace();
             }
         }
-
     }
 
     @Override
--- src/Log.java
+++ src/Log.java
@@ -8,41 +8,43 @@ public class Log implements Runnable {
     private ArrayList<Consumidor> consumidores;
     private Contenedor buffer_inicial;
     private Contenedor getBuffer_validados;
+    private int tiempo_ejecucion;
 
     public Log(ArrayList<Consumidor> consumidores, Contenedor buffer_inicial, Contenedor buffer_validados){
         this.consumidores = consumidores;
         this.buffer_inicial = buffer_inicial;
         this.getBuffer_validados = buffer_validados;
+        tiempo_ejecucion = 0;
     }
 
-    private void imprimirLog (){
+    private void imprimirLog (PrintWriter pw){
         int datos_totales=0;
-        int tiempo_ejecucion = 0;
-        try(FileWriter file=new FileWriter(".\\data\\log.txt");
-            PrintWriter pw=new PrintWriter(file);){
-            pw.printf("%s segundos de ejecución \n", tiempo_ejecucion);
-            for(int i = 0 ; i<consumidores.size() ; i++){   // consumidores.size == 2 siempre
-                pw.printf("Se procesaron %d datos del consumidor %d \n",consumidores.get(i).getDatos_procesados(),i);
-                datos_totales += consumidores.get(i).getDatos_procesados();
-            }
-            pw.printf("Se procesaron %d datos en total \n",datos_totales);
-            tiempo_ejecucion += 2;
-            pw.printf("%s segundos de ejecución \n", tiempo_ejecucion);pw.printf("El buffer inicial tiene %d datos almacenados, y el buffer de validados tiene %d datos almacenados \n\n" , buffer_inicial.getCantDeDatos() , getBuffer_validados.getCantDeDatos());
-        }catch(IOException e){e.printStackTrace();}
+        pw.printf("%s segundos de ejecución \n", tiempo_ejecucion);
+        for(int i = 0 ; i<consumidores.size() ; i++){
+            pw.printf("Se procesaron %d datos del consumidor %d \n",consumidores.get(i).getDatos_procesados(),i);
+            datos_totales += consumidores.get(i).getDatos_procesados();
+        }
+        pw.printf("Se procesaron %d datos en total \n",datos_totales);
+        pw.printf("El buffer inicial tiene %d datos almacenados, y el buffer de validados tiene %d datos almacenados \n\n" , buffer_inicial.getCantDeDatos() , getBuffer_validados.getCantDeDatos());
+        tiempo_ejecucion += 2;
     }
 
     @Override
     public void run(){
-        try(FileWriter file=new FileWriter(".\\data\\log.txt"); PrintWriter pw=new PrintWriter(file);){
-            while(true){
+        try(FileWriter file = new FileWriter("./data/log.txt");PrintWriter pw = new PrintWriter(file);){
+            while (true) {
                 try {
-                    TimeUnit.MILLISECONDS.sleep(2000);
-                    imprimirLog();
+                   Thread.sleep(2000);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                   e.printStackTrace();
                 }
+                imprimirLog(pw);
+                pw.flush();
             }
-        }catch(IOException e){e.printStackTrace();}
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
+
 
 }
