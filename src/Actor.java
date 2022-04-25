85ea08d903fa2b76de15bc31ac52635f31073545
Fausto Lavezzari <faustolavezzari99@gmail.com>
2022-04-25 11:56:50 -0300

    V1.7 (codigo depurado)

--- src/Actor.java
+++ src/Actor.java
@@ -1,6 +1,6 @@
 public abstract class Actor {
-    protected int timer1;
-    protected int id;
+    protected int timer1;     //atributo que indica el tiempo de procesamiento de datos que tendra cada hilo
+    protected int id;         //atributo que sirve como identificador para cada hilo
 
     public Actor(int timer1, int id){
         this.timer1 = timer1;
--- src/Main.java
+++ src/Main.java
@@ -3,44 +3,44 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        Thread[] creadores = new Thread[4];
-        Thread[] revisores = new Thread[2];
-        Thread[] consumidores = new Thread[2];
-        ArrayList<Consumidor> consures = new ArrayList<Consumidor>();
+        Thread[] creadores = new Thread[4];                  //Creamos un arreglo de tamaño 4 para los hilos creadores
+        Thread[] revisores = new Thread[2];                  //Creamos un arreglo de tamaño 2 para los hilos revisores
+        Thread[] consumidores = new Thread[2];               //Creamos un arreglo de tamaño 4 para los hilos consumidores
+        ArrayList<Consumidor> consures = new ArrayList<Consumidor>(); //Creamos un ArrayList para el log
 
-        Contenedor inicial = new Contenedor(0);
-        Contenedor validados = new Contenedor(1);
+        Contenedor inicial = new Contenedor(0);           //Creamos el buffer inicial instanciando un objeto de contenedor
+        Contenedor validados = new Contenedor(1);         //Creamos el buffer de validados instanciando un objeto de contenedor
 
-        for(int i=0;i<4;i++){
+        for(int i=0;i<4;i++){                               //utilizamos un ciclo for para poder instanciar los 4 hilos creadores
             Creador c = new Creador(i, inicial);
-            Thread t = new Thread(c,"Hilo Creador "+i);
+            Thread t = new Thread(c,"Hilo Creador "+i);//como Creadores implementa la interfaz runnable, la utilizamos para instanciar los hilos
             creadores[i] = t;
         }
 
-        for(int i=0; i<2; i++){
+        for(int i=0; i<2; i++){                                      //utilizamos un ciclo for para poder instanciar los 2 hilos revisores
             Revisor revisor = new Revisor(i, inicial, validados);
             Thread t = new Thread(revisor,"Hilo Revisor "+ i);  //como Revisores implementa la interfaz runnable, la utilizamos para instanciar los hilos
             revisores[i] = t;
 
-            Consumidor consumidor = new Consumidor(i, validados, inicial);
-            Thread cons = new Thread(consumidor,"Hilo Consumidor "+ i);
+            Consumidor consumidor = new Consumidor(i, validados, inicial);    //utilizamos un ciclo for para poder instanciar los 2 hilos consumidores
+            Thread cons = new Thread(consumidor,"Hilo Consumidor "+ i);  //como Consumidores implementa la interfaz runnable, la utilizamos para instanciar los hilos
             consumidores[i] = cons;
             consures.add(consumidor);
         }
-        Thread tlog = new Thread(new Log(consures, inicial, validados), "Hilo LOG ");
+        Thread tlog = new Thread(new Log(consures, inicial, validados), "Hilo LOG ");  //instanciamos el hilo Log
 
         // lanzamos todos los hilos
-       tlog.start();
+       tlog.start();               //iniciamos el hilo Log desde un principio para que no se pierda ningun dato durante la impresion
        for(int i=0; i<4; i++){
-           creadores[i].start();
+           creadores[i].start();   //iniciamos los hilos creadores
            if (i<2){
-               revisores[i].start();
-               consumidores[i].start();
+               revisores[i].start();  //iniciamos los hilos revisores
+               consumidores[i].start(); //iniciamos los hilos consumidores
            }
        }
 
         // esperamos a que los hilos finalicen
-        for(int i=0; i<4; i++){
+        for(int i=0; i<4; i++){               //en este punto del programa frenamos el main hasta que los demas hilos terminen de ejecutarse
             try{
                 creadores[i].join();
                 if (i<2){
