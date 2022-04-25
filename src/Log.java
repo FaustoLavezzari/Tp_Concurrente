83239324aa600ce75b707f445e4f1d21c7dcb7de
AgusMaff <agustinmaffini99@gmail.com>
2022-04-25 14:40:14 -0300

    V1.8.9 (haciendo el log)

--- src/Log.java
+++ src/Log.java
@@ -16,30 +16,33 @@ public class Log implements Runnable {
     }
 
     private void imprimirLog (){
-
         int datos_totales=0;
-        for(int i = 0 ; i<consumidores.size() ; i++){
-            System.out.printf("Se procesaron %d datos del consumidor %d \n",consumidores.get(i).getDatos_procesados(),i);
-            datos_totales += consumidores.get(i).getDatos_procesados();
-        }
-        System.out.printf("Se procesaron %d datos en total \n",datos_totales);
-        System.out.printf("El buffer inicial tiene %d datos almacenados, y el buffer de validados tiene %d datos almacenados \n\n" , buffer_inicial.getCantDeDatos() , getBuffer_validados.getCantDeDatos());
+        int tiempo_ejecucion = 0;
+        try(FileWriter file=new FileWriter(".\\data\\log.txt");
+            PrintWriter pw=new PrintWriter(file);){
+            pw.printf("%s segundos de ejecución \n", tiempo_ejecucion);
+            for(int i = 0 ; i<consumidores.size() ; i++){   // consumidores.size == 2 siempre
+                pw.printf("Se procesaron %d datos del consumidor %d \n",consumidores.get(i).getDatos_procesados(),i);
+                datos_totales += consumidores.get(i).getDatos_procesados();
+            }
+            pw.printf("Se procesaron %d datos en total \n",datos_totales);
+            tiempo_ejecucion += 2;
+            pw.printf("%s segundos de ejecución \n", tiempo_ejecucion);pw.printf("El buffer inicial tiene %d datos almacenados, y el buffer de validados tiene %d datos almacenados \n\n" , buffer_inicial.getCantDeDatos() , getBuffer_validados.getCantDeDatos());
+        }catch(IOException e){e.printStackTrace();}
     }
 
     @Override
     public void run(){
-        int tiempo_ejecucion = 0;
-        while(true){
-            try {
-                TimeUnit.MILLISECONDS.sleep(2000);
-                tiempo_ejecucion += 2;
-                System.out.printf("%s segundos de ejecución \n", tiempo_ejecucion);
-                imprimirLog();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        try(FileWriter file=new FileWriter(".\\data\\log.txt"); PrintWriter pw=new PrintWriter(file);){
+            while(true){
+                try {
+                    TimeUnit.MILLISECONDS.sleep(2000);
+                    imprimirLog();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }
+        }catch(IOException e){e.printStackTrace();}
     }
-
 
 }
